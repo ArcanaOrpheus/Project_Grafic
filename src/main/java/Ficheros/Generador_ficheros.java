2de716d14c95cfe59a7a9ea85fe8261a37f6aaea
ies <ies@1-6-12>
2018-11-27 15:27:58 +0100

    Started files

---
+++ src/main/java/Ficheros/Generador_ficheros.java
@@ -0,0 +1,32 @@
+package Ficheros;
+
+import project.*;
+import java.io.File;
+
+public class Generador_ficheros {
+	File fclient = new File("clients");
+	File fproveidor = new File("proveidors");
+	File fproductes = new File("productes");
+	//Serializacion
+	void ClientSave() {
+		
+	}
+	
+	void ClientRead() {
+		
+	}
+	void ProveidorSave() {
+		
+	}
+	
+	void ProveidorRead() {
+		
+	}
+	void ProducteSave() {
+		
+	}
+	
+	void ProducteRead() {
+		
+	}
+}
