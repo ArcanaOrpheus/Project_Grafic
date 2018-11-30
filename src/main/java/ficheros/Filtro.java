c7887157fd9afa8fffded4ab7c12b5bfce7192f4
ies <ies@info16-1>
2018-11-30 18:44:01 +0100

    Filtros 2.0

--- src/main/java/ficheros/Filtro.java
+++ src/main/java/ficheros/Filtro.java
@@ -10,6 +10,7 @@ import project.ComandaEstat;
 import project.Magatzem;
 import project.Producte;
 import project.Proveidor;
+import project.Tipus;
 
 
 public class Filtro {
@@ -47,7 +48,7 @@ public class Filtro {
 		return comandes;
 	}
 	
-	public ArrayList<Comanda> comandaDataLl(Magatzem m, Date d) {
+	public ArrayList<Comanda> comandaDataLl(Magatzem m, Date d, int i) {
 		
 		ArrayList<Comanda> comandes = new ArrayList<Comanda>();
 		
@@ -55,26 +56,63 @@ public class Filtro {
 				
 		for(Comanda ca : allcomandes)
 		{
-			if(ca.getDataLliurament() == d)
-			{
-				comandes.add(ca);
+			switch(i) {
+			case 0:
+				if(ca.getDataLliurament() == d)
+				{
+					comandes.add(ca);
+				}
+				break;
+			case 1:
+				if(ca.getDataLliurament().compareTo(d) >= 0)
+				{
+					comandes.add(ca);
+				}
+				break;
+			case 2:
+				if(ca.getDataLliurament().compareTo(d) <= 0)
+				{
+					comandes.add(ca);
+				}
+				break;
+				
 			}
+			
+			
 		}
 		
 		return comandes;
 	}
 	
-	public ArrayList<Comanda> comandaDataC(Magatzem m, Date d) {
+	public ArrayList<Comanda> comandaDataC(Magatzem m, Date d, int i) {
 		
 		ArrayList<Comanda> comandes = new ArrayList<Comanda>();
 		
 		List<Comanda> allcomandes = m.getComandes();
+		
 				
 		for(Comanda ca : allcomandes)
 		{
-			if(ca.getDataComanda() == d)
-			{
-				comandes.add(ca);
+			switch(i) {
+			case 0:
+				if(ca.getDataComanda() == d)
+				{
+					comandes.add(ca);
+				}
+				break;
+			case 1:
+				if(ca.getDataComanda().compareTo(d) >= 0)
+				{
+					comandes.add(ca);
+				}
+				break;
+			case 2:
+				if(ca.getDataComanda().compareTo(d) <= 0)
+				{
+					comandes.add(ca);
+				}
+				break;
+				
 			}
 		}
 		
@@ -99,7 +137,77 @@ public class Filtro {
 		return productes;
 	}
 	
+	public ArrayList<Producte> productePerTipus(Magatzem m, Tipus t)
+	{
+		
+		ArrayList<Producte> productes = new ArrayList<Producte>();
+		
+		List<Producte> allproductes = m.getProductes();
+		
+		for (Producte pr : allproductes)
+		{
+			if(pr.getTipus() == t)
+			{
+				productes.add(pr);
+			}
+		}
+		
+		return productes;
+	}
+	
+	public ArrayList<Producte> producteLessStock(Magatzem m){
+		
+		ArrayList<Producte> productes = new ArrayList<Producte>();
+		
+		List<Producte> allproductes = m.getProductes();
+		
+		for (Producte pr : allproductes)
+		{
+			if(pr.getStock() < pr.getStockMinim())
+			{
+				productes.add(pr);
+			}
+		}
+		return productes;
+	}
+	
+	public ArrayList<Producte> productePerPreu(Magatzem m, int i){
+		
+		ArrayList<Producte> productes = new ArrayList<Producte>();
+		
+		List<Producte> allproductes = m.getProductes();
+		
+		for (Producte pr : allproductes) {
+			switch(i) {
+			case 0:
+				if(pr.getPreuVenda() > i) {
+					productes.add(pr);
+				}
+				break;
+			case 1:
+				if(pr.getPreuVenda() < i) {
+					productes.add(pr);
+				}
+				break;
+			}
+		}
+		
+		return productes;
+	}
 	
 	
+	
+	/*
+	 * switch(i) {
+			case 0:
+				
+				break;
+			case 1:
+				break;
+			case 2:
+				break;
+				
+			}
+	 */
 	
 }
